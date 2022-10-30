bddc17a8734eb0c603d793d6f989d6d5d2fdc7e2
mk <mk.eng1119@gmail.com>
2022-10-30 14:12:36 +0900

    add: colors

--- src/styles/config/colors.ts
+++ src/styles/config/colors.ts
@@ -1,1 +1,9 @@
-export const COLORS = {};
+export const COLORS = {
+  white: "#f8f8ff",
+  beige: "#F9FBE7",
+  orange: "#FFAB40",
+  gray: "#757575",
+  blue: "#0277BD",
+  green: "#00838F",
+  red: "#EF5350",
+};
