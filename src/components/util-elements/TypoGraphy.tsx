988352d76aa000e8d95c7f13fe763ae955e589d8
mk <mk.eng1119@gmail.com>
2022-10-31 21:26:09 +0900

    add: TypoGraphy component

---
+++ src/components/util-elements/TypoGraphy.tsx
@@ -0,0 +1,43 @@
+import { SerializedStyles, css } from "@emotion/react";
+
+type Map = {
+  key: string;
+  class: SerializedStyles;
+};
+
+type Props = {
+  variant: "h1" | "h2" | "h3" | "h4" | "large" | "medium" | "small";
+  children: React.ReactNode;
+};
+
+export const TypoGraphy: React.FC<Props> = ({ variant, children }) => {
+  const variantSetting: Map[] = [
+    { key: "h1", class: css({ fontSize: "32px", fontWeight: "bold" }) },
+    { key: "h2", class: css({ fontSize: "28px", fontWeight: "bold" }) },
+    { key: "h3", class: css({ fontSize: "24px", fontWeight: "bold" }) },
+    { key: "h4", class: css({ fontSize: "20px", fontWeight: "bold" }) },
+    { key: "large", class: css({ fontSize: "20px" }) },
+    { key: "medium", class: css({ fontSize: "16px" }) },
+    { key: "small", class: css({ fontSize: "12px" }) },
+  ];
+
+  const variantStyle = variantSetting.find(
+    (setting) => setting.key === variant
+  )?.class;
+
+  return (
+    <>
+      {variant === "h1" ? (
+        <h1 css={variantStyle}>{children}</h1>
+      ) : variant === "h2" ? (
+        <h2 css={variantStyle}>{children}</h2>
+      ) : variant === "h3" ? (
+        <h3 css={variantStyle}>{children}</h3>
+      ) : variant === "h4" ? (
+        <h4 css={variantStyle}>{children}</h4>
+      ) : (
+        <div css={variantStyle}>{children}</div>
+      )}
+    </>
+  );
+};
