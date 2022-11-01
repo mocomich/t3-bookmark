25dfe3f231c5ef8f49ce612f766953643855e224
mk <mk.eng1119@gmail.com>
2022-11-01 22:25:36 +0900

    add: utils

--- src/styles/config/colors.ts
+++
@@ -1,9 +0,0 @@
-export const COLORS = {
-  white: "#f8f8ff",
-  beige: "#F9FBE7",
-  orange: "#FFAB40",
-  gray: "#757575",
-  blue: "#0277BD",
-  green: "#00838F",
-  red: "#EF5350",
-};
---
+++ src/styles/config/utils.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  white: "#fff",
+  offWhite: "#f8f8ff",
+  beige: "#F9FBE7",
+  orange: "#FFAB40",
+  gray: "#757575",
+  blue: "#0277BD",
+  green: "#00838F",
+  red: "#EF5350",
+};
+
+export const BOX_SHADOW = {
+  md: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+};
