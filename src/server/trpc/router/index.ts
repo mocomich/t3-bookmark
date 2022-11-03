e6168a6b819b18cc41159a76a19c8d02cefcb344
mk <mk.eng1119@gmail.com>
2022-11-03 21:42:39 +0900

    init

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,12 +1,5 @@
-// src/server/trpc/router/index.ts
 import { t } from "../trpc";
-import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 
-export const appRouter = t.router({
-  example: exampleRouter,
-  auth: authRouter,
-});
+export const appRouter = t.router({});
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
