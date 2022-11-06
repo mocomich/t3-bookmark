55d5e2a8d51b9fc9a4e593f5b022225d3666623f
mk <mk.eng1119@gmail.com>
2022-11-06 09:59:00 +0900

    update: color offWhite

--- src/components/util-elements/Button.tsx
+++ src/components/util-elements/Button.tsx
@@ -1,21 +1,26 @@
-import { COLORS } from "@/styles/config/utils";
+import { BOX_SHADOW, COLORS } from "@/styles/config/utils";
 import { css } from "@emotion/react";
 import { memo } from "react";
 
 type Props = {
   children: React.ReactNode;
   size: "small" | "medium" | "large";
-  color?: "gray" | "blue" | "green" | "red";
+  color?: "gray" | "blue" | "green" | "red" | "offWhite";
   onClick?: () => void;
 };
 
 export const Button: React.FC<Props> = memo(
   ({ children, size, color = "green", onClick }) => {
     const baseStyle = css({
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
       width: "100%",
       borderRadius: "6px",
-      color: COLORS.white,
+      color: color === "offWhite" ? "black" : COLORS.white,
       background: COLORS[color],
+      border: color === "offWhite" ? `1px solid ${COLORS.lightGray}` : "",
+      boxShadow: BOX_SHADOW.md,
     });
 
     const styles = {
