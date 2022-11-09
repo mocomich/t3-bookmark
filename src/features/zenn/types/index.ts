b7eae81a7a6b54053ddd21a27d67f1a9b06efd21
mk <mk.eng1119@gmail.com>
2022-11-09 20:42:23 +0900

    add: type

---
+++ src/features/zenn/types/index.ts
@@ -0,0 +1,30 @@
+export type ZennArticle = {
+  id: number;
+  postType: string;
+  title: string;
+  slug: string;
+  published: boolean;
+  commentsCount: number;
+  likedCount: number;
+  bodyLettersCount: number;
+  articleType: string;
+  emoji: string;
+  isSuspendingPrivate: boolean;
+  publishedAt: Date;
+  bodyUpdatedAt: string | null;
+  sourceRepoUpdatedAt: string | null;
+  path: string;
+  user: {
+    id: number;
+    username: string;
+    name: string;
+    avatarSmallUrl: string;
+  };
+  publication: {
+    id: number;
+    name: string;
+    avatarSmallUrl: string;
+    displayName: string;
+    avatarRegistered: boolean;
+  } | null;
+};
