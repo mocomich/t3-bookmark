b63a7cbf4625d32c9ea8ed6207dcecc00f951394
mk <mk.eng1119@gmail.com>
2022-11-11 13:29:30 +0900

    update: zenn articles

---
+++ src/features/zenn/components/Zenn.tsx
@@ -0,0 +1,35 @@
+import { TypoGraphy } from "@/components/util-elements/TypoGraphy";
+import { ErrorFallback } from "@/components/util-parts/ErrorBoundary";
+import { css } from "@emotion/react";
+import { Suspense, memo } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { ZennArticleList } from "./ZennArticleList";
+
+export const Zenn: React.FC = memo(() => {
+  return (
+    <div css={styles.container}>
+      <div className='mt-8'>
+        <TypoGraphy variant='h2'>
+          <a href='https://zenn.dev' target='_blank' rel='noopener noreferrer'>
+            Zenn Articles
+          </a>
+        </TypoGraphy>
+      </div>
+      <div className='mt-8'>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Suspense fallback={<>Loading...</>}>
+            <ZennArticleList />
+          </Suspense>
+        </ErrorBoundary>
+      </div>
+    </div>
+  );
+});
+
+const styles = {
+  container: css({
+    width: "80%",
+    margin: "0 auto",
+  }),
+};
