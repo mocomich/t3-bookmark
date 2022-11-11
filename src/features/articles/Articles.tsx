ed8343855474635607d1906fe039929880793a23
mk <mk.eng1119@gmail.com>
2022-11-12 01:45:16 +0900

    add:articles

---
+++ src/features/articles/Articles.tsx
@@ -0,0 +1,14 @@
+import { Tab } from "@/components/util-parts/Tab/Tab";
+import React from "react";
+
+import { Qiita } from "../qiita/components/Qiita";
+import { Zenn } from "../zenn/components/Zenn";
+
+const tabs = [
+  { title: "Zenn", key: "zenn", component: <Zenn /> },
+  { title: "Qiita", key: "qiita", component: <Qiita /> },
+] as const;
+
+export const Articles: React.FC = () => {
+  return <Tab defaultKey='zenn' tabs={tabs} />;
+};
