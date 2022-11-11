01f65796d19264861b21f7d47f1cddc44aad4c2e
mk <mk.eng1119@gmail.com>
2022-11-12 01:45:57 +0900

    add: qiita query hooks

---
+++ src/features/qiita/hooks/useQueryQiitaArticles.ts
@@ -0,0 +1,23 @@
+import { env } from "@/env/client.mjs";
+import { QiitaArticleType } from "@/features/qiita/types";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export const useQueryQiitaArticles = () => {
+  const url = `${env.NEXT_PUBLIC_ENDPOINT}/api/qiita/getArticles`;
+
+  const getArticles = async () => {
+    const { data } = await axios.get<QiitaArticleType[]>(url);
+    if (!data) throw new Error();
+
+    return data;
+  };
+
+  return useQuery<QiitaArticleType[], Error>({
+    queryKey: ["qiita"],
+    queryFn: getArticles,
+    refetchOnWindowFocus: true,
+    staleTime: 0,
+    suspense: true,
+  });
+};
