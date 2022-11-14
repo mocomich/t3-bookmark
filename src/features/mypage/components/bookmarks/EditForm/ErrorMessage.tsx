7c40bb30e50e7f96b8a7950b8e59ca05a6724976
mk <mk.eng1119@gmail.com>
2022-11-14 18:40:46 +0900

    add: memo

--- src/features/mypage/components/bookmarks/EditForm/ErrorMessage.tsx
+++ src/features/mypage/components/bookmarks/EditForm/ErrorMessage.tsx
@@ -1,9 +1,10 @@
 import { TypoGraphy } from "@/components/util-elements/TypoGraphy";
+import { memo } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export const ErrorMessage: React.FC<Props> = ({ children }) => {
+export const ErrorMessage: React.FC<Props> = memo(({ children }) => {
   return <TypoGraphy variant='error'>{children}</TypoGraphy>;
-};
+});
