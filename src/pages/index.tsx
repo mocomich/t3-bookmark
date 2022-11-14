62264600ac0d1a8adbe264c54420bc2415d83a5e
mk <mk.eng1119@gmail.com>
2022-11-14 19:32:12 +0900

    update: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { ArticleList } from "@/components/bookmark/ArticleList";
+import { Space } from "@/components/util-elements/Space";
+import { Articles } from "@/features/articles/Articles";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <section>
-      <ArticleList />
+      <Articles />
+      <Space axis='VERTICAL' size={40} />
     </section>
   );
 };
