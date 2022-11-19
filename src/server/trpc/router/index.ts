5d094b4a286e866a667032fbe76e2a5a9230e246
mk <mk.eng1119@gmail.com>
2022-11-19 22:46:49 +0900

    add: router

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,8 +1,10 @@
 import { t } from "../trpc";
+import { bookmarkRouter } from "./bookmark";
 import { categoryRouter } from "./category";
 
 export const appRouter = t.router({
   category: categoryRouter,
+  bookmark: bookmarkRouter,
 });
 
 export type AppRouter = typeof appRouter;
