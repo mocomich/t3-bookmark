4d6f77d27ffb7f6a8ca1dfaff1fc769460c59c6a
mk <mk.eng1119@gmail.com>
2022-11-20 17:07:50 +0900

    add: links

---
+++ src/features/mypage/const/index.ts
@@ -0,0 +1,11 @@
+export const links = [
+  { path: "/mypage/bookmarks/all", title: "All Bookmarks" },
+  {
+    path: "/mypage/bookmarks/unread",
+    title: "UnRead",
+  },
+  {
+    path: "/mypage/bookmarks/read",
+    title: "Already Read",
+  },
+] as const;
