e5850031479450eea8098f37655c55180682bec7
mk <mk.eng1119@gmail.com>
2022-11-23 18:53:32 +0900

    add: const PATHS

--- src/features/mypage/const/index.ts
+++ src/features/mypage/const/index.ts
@@ -1,14 +1,20 @@
+export const PATH_LIST = {
+  all: "/mypage/bookmarks/all",
+  unread: "/mypage/bookmarks/unread",
+  read: "/mypage/bookmarks/read",
+} as const;
+
 export const links = [
-  { path: "/mypage/bookmarks/all", title: "All Bookmarks" },
+  { path: PATH_LIST["all"], title: "All Bookmarks" },
   {
-    path: "/mypage/bookmarks/unread",
+    path: PATH_LIST["unread"],
     title: "UnRead",
   },
   {
-    path: "/mypage/bookmarks/read",
+    path: PATH_LIST["read"],
     title: "Already Read",
   },
 ] as const;
 
 // TODO: 挙動確認の為一時的に6、後程9に変更する
-export const DEFAULT_LIMIT = 6;
+export const DEFAULT_LIMIT = 3;
