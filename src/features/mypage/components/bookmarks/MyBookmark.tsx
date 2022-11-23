024879eb51f582da316cc209502d705843b67692
mk <mk.eng1119@gmail.com>
2022-11-23 20:58:24 +0900

    add: AllMyBookmarks components

---
+++ src/features/mypage/components/bookmarks/MyBookmark.tsx
@@ -0,0 +1,75 @@
+import { TypoGraphy } from "@/components/util-elements/TypoGraphy";
+import { UtilLink } from "@/components/util-elements/UtilLink";
+import { BOX_SHADOW } from "@/styles/config/utils";
+import { getDateMMdd } from "@/utils/libs";
+import { css } from "@emotion/react";
+import Image from "next/image";
+import { memo } from "react";
+
+type Props = {
+  imageId: number;
+  id: string;
+  url: string;
+  title: string;
+  updatedAt: Date;
+};
+
+export const MyBookmark: React.FC<Props> = memo(
+  ({ id, imageId, url, title, updatedAt }) => {
+    // TODO: 詳細ページ（メモ）のリンク設置
+    return (
+      <article css={styles.container}>
+        <UtilLink style={styles.head} href={url}>
+          <div css={styles.headImage}>
+            <Image
+              src={`/assets/dogs/dog${imageId}.png`}
+              layout='fill'
+              objectFit='contain'
+              alt='犬のアイコン画像'
+            />
+          </div>
+        </UtilLink>
+        <UtilLink style={styles.content} href={url}>
+          <TypoGraphy variant='h4' isResponsive>
+            {title}
+          </TypoGraphy>
+        </UtilLink>
+        <div css={styles.bottom}>
+          <TypoGraphy variant='small'>{getDateMMdd(updatedAt)}</TypoGraphy>
+        </div>
+      </article>
+    );
+  }
+);
+
+const styles = {
+  container: css({
+    width: "100%",
+    background: "white",
+    boxShadow: BOX_SHADOW.lg,
+    display: "grid",
+    gridTemplateRows: "140px 2fr 1fr",
+    minHeight: "280px",
+    borderRadius: "12px",
+  }),
+  head: css({
+    background: "#71abb3",
+    position: "relative",
+    borderRadius: "6px 6px 0 0",
+  }),
+  headImage: css({
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "50px",
+    height: "50px",
+  }),
+  content: css({
+    padding: "12px",
+  }),
+  bottom: css({
+    padding: "12px",
+    justifySelf: "flex-end",
+  }),
+};
---
+++ src/features/mypage/components/bookmarks/all/AllMyBookmarks.tsx
@@ -0,0 +1,45 @@
+import { Space } from "@/components/util-elements/Space";
+import { ErrorFallback } from "@/components/util-parts/ErrorBoundary";
+import { PulseArticleList } from "@/components/util-parts/PulseArticleList";
+import { Pagination } from "@/components/util-parts/pagination/Pagination";
+import { PATH_LIST } from "@/features/mypage/const";
+import { usePagination } from "@/features/mypage/hooks/usePagination";
+import { trpc } from "@/utils/trpc";
+import { css } from "@emotion/react";
+import { Suspense, memo } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { AllMyBookmarkList } from "./AllMyBookmarkList";
+
+export const AllMyBookmarks = memo(() => {
+  const { data: count } = trpc.bookmark.getCountAllBookmarksByUserId.useQuery();
+
+  const { isDisplayButton, page, handlePaginationChange } = usePagination(
+    PATH_LIST.all,
+    count
+  );
+  return (
+    <section css={styles.container}>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<PulseArticleList count={6} />}>
+          <AllMyBookmarkList currentPage={page} />
+        </Suspense>
+      </ErrorBoundary>
+      <Space axis='VERTICAL' size={40} />
+      <Pagination
+        page={page}
+        onClick={handlePaginationChange}
+        isDisplayButtonPrev={isDisplayButton(page).prev}
+        isDisplayButtonNext={isDisplayButton(page).next}
+      />
+    </section>
+  );
+});
+
+const styles = {
+  container: css({
+    maxWidth: "1120px",
+    width: "80%",
+    margin: "0 auto",
+  }),
+};
