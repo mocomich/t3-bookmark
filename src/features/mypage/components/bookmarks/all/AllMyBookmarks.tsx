e1e8b1cbcb539207c6f1f5b9bff943b7a5d86a4a
mk <mk.eng1119@gmail.com>
2022-11-24 20:35:20 +0900

    fix: import

--- src/features/mypage/components/bookmarks/all/AllMyBookmarks.tsx
+++ src/features/mypage/components/bookmarks/all/AllMyBookmarks.tsx
@@ -1,7 +1,7 @@
 import { Space } from "@/components/util-elements/Space";
 import { ErrorFallback } from "@/components/util-parts/ErrorBoundary";
-import { PulseArticleList } from "@/components/util-parts/PulseArticleList";
 import { Pagination } from "@/components/util-parts/pagination/Pagination";
+import { PulseArticleList } from "@/components/util-parts/pulse/PulseArticleList";
 import { PATH_LIST } from "@/features/mypage/const";
 import { usePagination } from "@/features/mypage/hooks/usePagination";
 import { trpc } from "@/utils/trpc";
