eb4b1724049af0799f42972e0d2376156b87951f
mk <mk.eng1119@gmail.com>
2022-11-24 17:25:57 +0900

    remove: not using

--- src/features/mypage/components/bookmarks/links/MyBookmarkList.tsx
+++
@@ -1,9 +0,0 @@
-import { trpc } from "@/utils/trpc";
-
-export const MyBookmarkList: React.FC = () => {
-  const { data: bookmarks } = trpc.bookmark.getBookmarksByUserId.useQuery();
-
-  console.log(bookmarks, "book");
-
-  return <div>MyBookmarkList</div>;
-};
