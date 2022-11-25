c4fc4e3533dd4c863e292f40a57b0c4995da93ed
mk <mk.eng1119@gmail.com>
2022-11-25 20:00:16 +0900

    fix: schema name

--- src/features/mypage/types/index.ts
+++ src/features/mypage/types/index.ts
@@ -4,7 +4,7 @@ import { MY_BOOKMARK_LINK_LIST } from "../const";
 import {
   createBookmarkSchema,
   deleteBookmarkSchema,
-  getBookmarkByIdSchema,
+  getSingleBookmarkByIdSchema,
   updateBookmarkSchema,
 } from "../schema";
 
@@ -13,7 +13,7 @@ export type FormDataType = CreateBookmarkType;
 export type CreateBookmarkType = z.infer<typeof createBookmarkSchema>;
 export type updateBookmarkType = z.infer<typeof updateBookmarkSchema>;
 export type DeleteBookmarkType = z.infer<typeof deleteBookmarkSchema>;
-export type GetSBookmarkByIdType = z.infer<typeof getBookmarkByIdSchema>;
+export type GetSBookmarkByIdType = z.infer<typeof getSingleBookmarkByIdSchema>;
 
 export type LinkPathsType = typeof MY_BOOKMARK_LINK_LIST[number]["path"];
 export type LinkTitlesType = typeof MY_BOOKMARK_LINK_LIST[number]["title"];
