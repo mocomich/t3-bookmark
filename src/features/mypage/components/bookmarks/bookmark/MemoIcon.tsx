c6ebe1a358a81d6dcf3593e46e533f712b08b79c
mk <mk.eng1119@gmail.com>
2022-11-25 17:40:58 +0900

    add: memo icon component

---
+++ src/features/mypage/components/bookmarks/bookmark/MemoIcon.tsx
@@ -0,0 +1,38 @@
+import { BOX_SHADOW, COLORS } from "@/styles/config/utils";
+import { css } from "@emotion/react";
+import { useRouter } from "next/router";
+import { memo } from "react";
+import { FiFileText } from "react-icons/fi";
+
+type Props = {
+  id: string;
+};
+export const MemoIcon: React.FC<Props> = memo(({ id }) => {
+  const router = useRouter();
+  const onClickTransitionPage = (
+    e: React.FormEvent<HTMLButtonElement>,
+    id: string
+  ) => {
+    e.preventDefault();
+    router.push(`/mypage/${id}`);
+  };
+  return (
+    <button css={styles.detail} onClick={(e) => onClickTransitionPage(e, id)}>
+      <FiFileText css={styles.icon} />
+    </button>
+  );
+});
+
+const styles = {
+  detail: css({
+    padding: "8px",
+    borderRadius: "6px",
+    background: COLORS.green,
+    display: "inline-block",
+    boxShadow: BOX_SHADOW.md,
+  }),
+  icon: css({
+    fontSize: "24px",
+    color: "white",
+  }),
+};
