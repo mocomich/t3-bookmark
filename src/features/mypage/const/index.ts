c4778cf08142c4a8dceb141161f666d5959f0d8e
mk <mk.eng1119@gmail.com>
2022-11-27 00:57:08 +0900

    add: setting path

--- src/features/mypage/const/index.ts
+++ src/features/mypage/const/index.ts
@@ -2,6 +2,7 @@ export const PATH_LIST = {
   all: "/mypage/bookmarks/all",
   unread: "/mypage/bookmarks/unread",
   read: "/mypage/bookmarks/read",
+  settings: "/mypage/settings",
 } as const;
 
 export const MY_BOOKMARK_LINK_LIST = [
@@ -14,6 +15,10 @@ export const MY_BOOKMARK_LINK_LIST = [
     path: PATH_LIST["read"],
     title: "Already Read",
   },
+] as const;
+
+export const SETTING_LINK_LIST = [
+  { path: PATH_LIST["settings"], title: "Settings" },
 ] as const;
 
 export const DEFAULT_LIMIT = 9;
