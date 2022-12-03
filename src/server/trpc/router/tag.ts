c9404ea99fb94c3b9a38f28d859642275a4eb4f2
mk <mk.eng1119@gmail.com>
2022-12-03 20:52:57 +0900

    add: tag router

---
+++ src/server/trpc/router/tag.ts
@@ -0,0 +1,39 @@
+import { createTagSchema, deleteTagSchema } from "@/features/mypage/schema";
+
+import { authedProcedure, t } from "../trpc";
+
+export const tagRouter = t.router({
+  getAllTags: authedProcedure.query(({ ctx }) => {
+    return ctx.prisma.tag.findMany({
+      where: {
+        userId: ctx.session?.user?.id,
+      },
+    });
+  }),
+
+  createTag: authedProcedure
+    .input(createTagSchema)
+    .mutation(async ({ ctx, input }) => {
+      const tag = await ctx.prisma.tag.create({
+        data: {
+          ...input,
+          user: {
+            connect: {
+              id: ctx.session?.user?.id,
+            },
+          },
+        },
+      });
+      return tag;
+    }),
+
+  deleteTag: authedProcedure
+    .input(deleteTagSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.tag.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+});
