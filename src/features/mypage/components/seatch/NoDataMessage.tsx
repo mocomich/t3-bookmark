f280d109a868b3cdf73dd2e0d499f588bf99db62
mk <mk.eng1119@gmail.com>
2022-12-04 18:36:11 +0900

    add:NoDataMessage component

---
+++ src/features/mypage/components/seatch/NoDataMessage.tsx
@@ -0,0 +1,27 @@
+import { TypoGraphy } from "@/components/util-elements/TypoGraphy";
+import { COLORS } from "@/styles/config/utils";
+import { css } from "@emotion/react";
+
+type Props = {
+  searchQuery: string;
+};
+export const NoDataMessage: React.FC<Props> = ({ searchQuery }) => {
+  return (
+    <div css={styles.container}>
+      <TypoGraphy variant='medium'>
+        {searchQuery}の検索結果が見つかりませんでした
+      </TypoGraphy>
+    </div>
+  );
+};
+
+const styles = {
+  container: css({
+    height: "40px",
+    textAlign: "center",
+    fontWeight: "bold",
+    display: "grid",
+    placeItems: "center",
+    color: COLORS.gray,
+  }),
+};
