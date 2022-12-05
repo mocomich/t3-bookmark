a68ed7ed31a9c0fd59148ad5834743886a64945e
mk <mk.eng1119@gmail.com>
2022-12-06 01:43:34 +0900

    change link title

--- src/components/layout/header/DropDownMenuContent.tsx
+++ src/components/layout/header/DropDownMenuContent.tsx
@@ -12,7 +12,7 @@ import { RiLogoutBoxRFill } from "react-icons/ri";
 const LinkRoot = [
   {
     href: "/mypage/bookmarks/all",
-    title: "My Page",
+    title: "My Bookmarks",
     icon: <IoBook size={18} className='mr-2 mt-1' />,
     index: 1,
   },
