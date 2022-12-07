f81a0bbb309f1ae7d7f17e0eff6b0540954aa1f2
mk <mk.eng1119@gmail.com>
2022-12-07 22:35:54 +0900

    add: util function

--- src/utils/libs/index.ts
+++ src/utils/libs/index.ts
@@ -13,3 +13,13 @@ export const getDateMMdd = (targetDate: Date) => {
 export const getDateFromNow = (targetDate: Date) => {
   return formatDistanceToNow(targetDate, { addSuffix: true, locale: ja });
 };
+
+export const getQuery = (query: string | string[] | undefined) => {
+  if (query === undefined) {
+    return "";
+  }
+  if (typeof query !== "string") {
+    throw new TypeError("正しい値を送信して下さい");
+  }
+  return query;
+};
