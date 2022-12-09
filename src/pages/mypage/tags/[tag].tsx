31a0deb9e11ca33b3066e04d8eb2d643be9278f6
mk <mk.eng1119@gmail.com>
2022-12-09 22:28:40 +0900

    add: tag result

---
+++ src/pages/mypage/tags/[tag].tsx
@@ -0,0 +1,20 @@
+import { Space } from "@/components/util-elements/Space";
+import { TopicTitle } from "@/features/categories/components/category/TopicTitle";
+import { TagSearchResult } from "@/features/tags/components/tags/TagSearchResult";
+import { useQueryBookmarks } from "@/features/tags/hooks/useQueryBookmarks";
+import { NextPage } from "next";
+
+const TagPage: NextPage = () => {
+  const { query } = useQueryBookmarks();
+
+  return (
+    <section>
+      <TopicTitle title={query} />
+      <Space axis='VERTICAL' size={40} />
+      <TagSearchResult />
+      <Space axis='VERTICAL' size={40} />
+    </section>
+  );
+};
+
+export default TagPage;
