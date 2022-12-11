f8d892ed661542a517b4fe4c6b93d6b0df95e4db
mk <mk.eng1119@gmail.com>
2022-12-11 09:26:08 +0900

    update: footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,12 +1,31 @@
+import { LAYOUT_WIDTH } from "@/styles/config/sizes";
+import { COLORS } from "@/styles/config/utils";
 import { css } from "@emotion/react";
 import { memo } from "react";
 
+import { Space } from "../util-elements/Space";
+import { Logo } from "./Logo";
+
 export const Footer: React.FC = memo(() => {
-  return <div css={styles.container}>Footer</div>;
+  return (
+    <div css={styles.container}>
+      <div css={styles.footer} />
+      <Space axis='VERTICAL' size={20} />
+      <Logo />
+    </div>
+  );
 });
 
 const styles = {
   container: css({
-    background: "blue",
+    background: COLORS.white,
+    display: "grid",
+    justifyItems: "center",
+    alignContent: "end",
+  }),
+  footer: css({
+    borderTop: `1px solid ${COLORS.lightGray}`,
+    maxWidth: LAYOUT_WIDTH,
+    width: "100%",
   }),
 };
