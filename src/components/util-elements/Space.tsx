542ff8ce4625b43fa35d7fbe99ee60038f0aba23
mk <mk.eng1119@gmail.com>
2022-12-11 09:26:48 +0900

    update: responsive

--- src/components/util-elements/Space.tsx
+++ src/components/util-elements/Space.tsx
@@ -1,8 +1,11 @@
+import { sp } from "@/styles/mixin";
+
 type Props = {
   size: number;
   axis: "VERTICAL" | "HORIZONTAL";
+  isResponsive?: boolean;
 };
-export const Space: React.FC<Props> = ({ size, axis }) => {
+export const Space: React.FC<Props> = ({ size, axis, isResponsive }) => {
   const width = axis === "VERTICAL" ? 1 : size;
   const height = axis === "HORIZONTAL" ? 1 : size;
   return (
@@ -13,6 +16,12 @@ export const Space: React.FC<Props> = ({ size, axis }) => {
         minWidth: width,
         height,
         minHeight: height,
+        [sp]: {
+          width: isResponsive ? width / 2 : width,
+          minWidth: isResponsive ? width / 2 : width,
+          height: isResponsive ? height / 2 : height,
+          minHeight: isResponsive ? height / 2 : height,
+        },
       }}
     />
   );
