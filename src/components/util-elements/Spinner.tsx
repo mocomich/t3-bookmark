859d373fd3d9ab7e140c50a93a448f68fff22c56
mk <mk.eng1119@gmail.com>
2022-12-14 19:23:11 +0900

    add: spinner component

---
+++ src/components/util-elements/Spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <div className='animate-spin h-10 w-10 border-4 border-green-700 rounded-full border-t-transparent'></div>
+    </div>
+  );
+};
