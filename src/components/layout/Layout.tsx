7b68ac7ea3bdb73f30e192598ab7e91d7702774d
mk <mk.eng1119@gmail.com>
2022-12-18 18:27:44 +0900

    add: title

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,23 @@
 import { FOOTER_HEIGHT, HEADER_HEIGHT } from "@/styles/config/sizes";
 import { COLORS } from "@/styles/config/utils";
 import { css } from "@emotion/react";
+import Head from "next/head";
 
 import { Footer } from "./Footer";
 import { Header } from "./header/Header";
 
 type Props = {
+  title: string;
   children: React.ReactNode;
 };
 
-export const Layout: React.FC<Props> = ({ children }) => {
+export const Layout: React.FC<Props> = ({ children, title }) => {
   return (
     <div css={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <meta property='og:title' content={title} />
+      </Head>
       <Header />
       <main css={styles.main}>{children}</main>
       <Footer />
